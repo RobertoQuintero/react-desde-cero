03fc0219c130e004d7fc1edb4b486411461de243
Roberto Quintero <langdonhp435@gmail.com>
2019-09-03 10:46:43 -0500

    Primer commit del curso React.js desde cero

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './styles/styles.scss';
+import Curso from './Curso'
+const App =()=> (
+  <>
+    <div className="main-banner img-container l-section" id="main-banner">
+  <div className="ed-grid lg-grid-6">
+    <div className="lg-cols-4 lg-x-2">
+      <img className="main-banner__img" src="https://images.pexels.com/photos/597909/pexels-photo-597909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="banner"/>
+      <div className="main-banner__data s-center">
+        <p className="t2 s-mb-0">Cursos de Edteam</p>
+        <p> Tu futuro te está esperando</p>
+        <a href="https://ed.team" target="blank" className="button">Suscribirse</a>
+      </div>
     </div>
-  );
-}
+  </div>
+</div>
+<div className="ed-grid m-grid-3">
+  <Curso />
+  <Curso />
+  <Curso />
+  <Curso />
+  <Curso />
+  <Curso />
+  <Curso />
+  <Curso />
+  <Curso />
+</div>
+  </>
+)
 
+
+//reglas
+// 1: Toda etiqueta debe cerrarse
+// 2: Los componentes deben devolver un solo elemento padre
+// 3: Apoyarse de los fragments cuando necesito devolver mas de un elemento
+// 4: nomenclatura Fragment => <>hijos</>
+// 5: img siempre se cierra
+// 6: className => class
+// 7: for => htmlFor
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// ReactDOM.render(<App />, document.getElementById('root'));
+const root = document.getElementById('root')
+  // elemento = React.createElement(componente,propiedades,hijos)
+  // elemento = React.createElement('h1',{claseName: 'saludo'},'hola mundo')
+// ReactDOM.render(elemento,root)
+ReactDOM.render(<App /> ,root)
+
+
+
+
+
